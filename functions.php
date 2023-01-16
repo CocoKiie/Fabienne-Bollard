3129a6d3907014d118eab19ad23a19dfffe13ef2
CocoKiie <122778707+CocoKiie@users.noreply.github.com>
2023-01-16 16:11:26 +0100

    rien

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 //Load css sur touuuuuuuuutes les pages
 function add_style() {
     wp_enqueue_style('main-style', get_template_directory_uri() . '/css/app.css', false);
-  }
-  add_action( 'wp_enqueue_scripts', 'add_style' );
+}
+add_action( 'wp_enqueue_scripts', 'add_style' );
 ?>
