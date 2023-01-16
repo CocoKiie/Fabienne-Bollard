e4750fcbd945a7b6bb58ffa247f6341ad2209345
CocoKiie <122778707+CocoKiie@users.noreply.github.com>
2023-01-16 10:48:54 +0100

    functions > add css link

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
 //Load css sur touuuuuuuuutes les pages
-wp_enqueue_style('app', get_template_directory_uri().'/css/app.css', array(), 1, 'all'); 
+function add_style() {
+    wp_enqueue_style('main-style', get_template_directory_uri() . '/css/app.css', false);
+  }
+  add_action( 'wp_enqueue_scripts', 'add_style' );
 ?>
