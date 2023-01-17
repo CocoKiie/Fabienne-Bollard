237861549eb488cea4ca256d8ffc545738173008
CocoKiie <122778707+CocoKiie@users.noreply.github.com>
2023-01-17 20:56:16 +0100

    bouton non personifiable

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
             <img src="../wp-content/themes/usmb/img/fabi_portrait_fun.png" alt="portrait de Fabienne Bollard" loading="lazy">
             <div>
                 <img src="../wp-content/themes/usmb/img/logo_white.svg" alt="logo de Fabienne Bollard" loading="lazy">
-                <button><i></i><?php the_field("rdv"); ?></button>
+                <button><i></i>Prendre rendez-vous</button>
                 <ul>
                     <li><i></i><a href="tel:0687800411">06.87.80.04.11</a></li>
                     <li><i></i><a href="https://goo.gl/maps/KojEJXZsCiJk8ePd6" target="_blank">464 Route des Près Rollier<br>Zone de Grand Epagny<br>74330 Sillingy</a></li>
