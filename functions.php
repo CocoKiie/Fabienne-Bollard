b6bb9c4a487ca420371c5872bf55874108bf8e14
CocoKiie <122778707+CocoKiie@users.noreply.github.com>
2023-01-18 11:54:38 +0100

    update consultation

--- functions.php
+++ functions.php
@@ -4,3 +4,10 @@ function add_style() {
     wp_enqueue_style('main-style', get_template_directory_uri() . '/css/app.css', false);
 }
 add_action( 'wp_enqueue_scripts', 'add_style' );
+
+function debug($var){
+    echo "<pre>";
+    var_dump($var);
+    echo "</pre>";
+    die();
+}
