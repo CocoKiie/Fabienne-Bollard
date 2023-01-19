fa376bdae893c1852419c597abd0393657e99647
CocoKiie <122778707+CocoKiie@users.noreply.github.com>
2023-01-19 11:38:02 +0100

    update page reflexe

---
+++ templates/page-reflexes.php
@@ -0,0 +1,17 @@
+<?php
+/*
+    Template Name:Reflexes
+    */
+
+//Chargement du header
+get_header();
+
+$image1 = get_field("image1");
+$image2 = get_field("image2");
+?>
+
+
+<?php
+//Chargement du footer
+get_footer();
+?>
